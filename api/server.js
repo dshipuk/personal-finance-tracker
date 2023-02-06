1db3290115e2105f42522bf089cea1bcccf1d68f
dshipuk <dshipuk@yahoo.com>
2023-02-05 16:20:27 -0800

    Init mongoDB and added error handling

--- api/server.js
+++ api/server.js
@@ -5,7 +5,12 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/personal-finance-tracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}); 
+});
 
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});
 
 module.exports = app;
