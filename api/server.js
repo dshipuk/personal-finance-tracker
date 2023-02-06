c588b251319e8650c1f2ebb0e90c483568fec6f3
dshipuk <dshipuk@yahoo.com>
2023-02-05 16:26:38 -0800

    updated package.json dev script to index.js from server.js and removed deprecated warning from mongoose strictQuery

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
