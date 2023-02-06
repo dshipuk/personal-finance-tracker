3e2fccb3abaeacb6e0723786ee5e327ead8ac618
dshipuk <dshipuk@yahoo.com>
2023-02-05 16:31:07 -0800

    Wrapped mongoose.connect in a try catch block to handle potential connection errors more gracefully

--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
-
 const express = require("express");
-const app = express();
+const { default: helmet } = require("helmet");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(cors());
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+try {
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (err) {
+  console.error(err);
+}
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
